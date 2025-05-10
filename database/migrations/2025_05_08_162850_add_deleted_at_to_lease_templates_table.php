9e2fb156849947e1bb1e718632e67fb6db43e147
Mpembee3 <mpembeesimon@gmail.com>
2025-05-10 16:25:14 +0300

    units table, leases templates migrations

---
+++ database/migrations/2025_05_08_162850_add_deleted_at_to_lease_templates_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('lease_templates', function (Blueprint $table) {
+            $table->softDeletes(); // Adds a nullable 'deleted_at' timestamp column
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('lease_templates', function (Blueprint $table) {
+            $table->dropSoftDeletes(); // Removes the 'deleted_at' column
+        });
+    }
+};
